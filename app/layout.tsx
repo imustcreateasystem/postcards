e28c7b4126577ec01ac7d6455de8b9a40fcdf7c1
imustcreateasystem <bankandshoaloftime@gmail.com>
2026-04-27 03:44:06 +0530

    feat: container component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/ui/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants/brand";
 import { ReactNode } from "react";
 import { sansSerifFont } from "@/ui/styles/fonts/fonts";
+import { cn } from "@/ui/utils/cn";
 
 export const metadata: Metadata = {
   title: APP_NAME,
@@ -15,7 +16,10 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className={`${sansSerifFont.variable} h-full antialiased`}>
+    <html
+      lang="en"
+      className={cn(sansSerifFont.variable, "h-full antialiased")}
+    >
       <body className="min-h-full">{children}</body>
     </html>
   );
---
+++ ui/utils/cn.ts
@@ -0,0 +1,13 @@
+/* Third-party imports. */
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Utility for merging and conditionally applying
+ * Tailwind CSS classes.
+ *
+ * @param inputs Union of class names to merge.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
