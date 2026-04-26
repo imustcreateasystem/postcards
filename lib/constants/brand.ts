cca34b9e8f1c3f937450bd5a88d66f382ed3b566
imustcreateasystem <bankandshoaloftime@gmail.com>
2026-04-27 02:41:08 +0530

    feat: cleaning up

---
+++ lib/constants/brand.ts
@@ -0,0 +1,10 @@
+/**
+ * The name of the app.
+ */
+export const APP_NAME = "Postcards";
+
+/**
+ * The description of the app.
+ */
+export const APP_DESCRIPTION =
+  "Curators recommend five things a week with short notes.";
